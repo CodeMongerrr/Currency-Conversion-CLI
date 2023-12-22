f1feea781bd44e7795ad3d7d2fa094993543ebf0
bitofcoding <92008241+bitofcoding@users.noreply.github.com>
2023-12-23 03:27:40 +0530

    created a cli structure

--- src/console.rs
+++ src/console.rs
@@ -1,1 +1,26 @@
 use crate::get_user_input;
+
+
+pub struct Console;
+
+impl Console {
+    pub fn clear(&self){
+        print!("\x1B[2J\x1B[1;1H");
+    }
+
+    pub fn conversion(&self){
+        println!("------------------------------------");
+        println!("---Welcome to Currency Conversion---");
+        println!("------------------------------------");
+        println!("-----Select your Base Currency------");
+        let base_currency = get_user_input().trim().to_string();
+        println!("--Fetching Selected Base Currency---");
+        println!("------------------------------------");
+        println!("------------------------------------");
+        println!("------------------------------------");
+        println!("--Select your Conversion Currency---");
+        let currency = get_user_input().trim().to_string();
+        println!("-------Conversion in Process--------");
+        println!("1 {} = {} {}", currency,,base_currency)
+    }
+}
